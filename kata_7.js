368133c3d0d08aec018b1550b3b97b0196fe95d2
Jonathan Tse <Jonathan@Jonathans-MBP.connect>
2020-04-23 21:27:34 +0100

    amending kata 7 solution

--- kata_7.js
+++ kata_7.js
@@ -2,13 +2,5 @@
 
 const sentence = 'The man fell off a cliff';
 
-// let reverseSentence = sentence.split(' ').reverse().join(' ');
-// console.log(reverseSentence);
-
-let reverseSentence = [];
-let sentenceArray = sentence.split(' ');
-console.log(sentenceArray);
-for (let i = sentenceArray.length - 1; i >= 0; i--) {
-    reverseSentence.push(sentenceArray[i]);
-}
+let reverseSentence = sentence.split(' ').reverse().join(' ');
 console.log(reverseSentence);
