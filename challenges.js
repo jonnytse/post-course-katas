0e928c80af73bc09fcd76eb7a596a730ed19bca6
Jonathan Tse <Jonathan@Jonathans-MBP.connect>
2020-04-23 16:14:24 +0100

    added challenges.js file

---
+++ challenges.js
@@ -0,0 +1,16 @@
+// How do you find the missing numbers in a given integer array?
+// How do you find the duplicate number on a given integer array?
+// How do you find duplicate numbers in an array if it contains multiple duplicates?
+// How do you remove duplicates from an array in place?
+// How do you find the largest and smallest number in an unsorted integer array?
+// How do you find all pairs of an integer array whose sum is equal to a given number?
+// How do you reverse the words in a sentence?
+// How do you print duplicate characters from a string?
+// How do you check if two strings are anagrams of each other?
+// How do you print the first non-repeated character from a string?
+// How do you check if a given string is a palindrome?
+// How can a given string be reversed using recursion?
+// How do you check if two strings are a rotation of each other?
+// How do you swap two numbers without using a third variable?
+// How do you find all permutations of a string?
+// How do you check if a string contains only digits?
